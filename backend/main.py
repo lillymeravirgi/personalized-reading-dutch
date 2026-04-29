c12c92d072f3acc27ee70523323e3f3f49f9720c
Virginia Simonetto <v.simonetto@student.maastrichtuniversity.nl>
2026-04-29 13:37:28 +0200

    fix the password issue

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,10 @@ app = FastAPI(
 # Allow the React dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
